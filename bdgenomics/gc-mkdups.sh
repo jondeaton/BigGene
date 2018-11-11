98221ca49b4c66f47956647407cf01edfecd5e9a
Jon Deaton <jonpauldeaton@gmail.com>
2018-11-10 22:17:33 -0800

    Added Google Cloud Dataproc submission script

---
+++ bdgenomics/gc-mkdups.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script submits a duplicate marking job to Google Cloud Dataproc
+# You will need to jave the jar: "google-cloud-nio-0.22.0-alpha-shaded.jar"
+# downloaded from this link and in the directory in which this file exists
+# http://central.maven.org/maven2/com/google/cloud/google-cloud-nio/0.22.0-alpha/
+# You will also need to have the adam repository in this same directory
+# You will probably also need to change the name of the cluster that you 
+# submit to
+
+ADAM_DIR="adam"
+ADAM_MAIN="org.bdgenomics.adam.cli.ADAMMain"
+ADAM_CLI_JAR=$(${ADAM_DIR}/bin/find-adam-assembly.sh)
+GC_NIO_JAR="google-cloud-nio-0.22.0-alpha-shaded.jar"
+
+gcloud dataproc jobs submit spark \
+  --cluster="bdgenomics-tester" \
+  --region="asia-east1" \
+  --class="$ADAM_MAIN" \
+  --jars="$ADAM_CLI_JAR,$GC_NIO_JAR" \
+  -- \
+  transformAlignments \
+  $1 $2 \
+  -mark_duplicate_reads \
+  -single \
+  -sort_reads
